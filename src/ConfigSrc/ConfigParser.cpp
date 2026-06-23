483e975efbc5a118df25020a32c2d124c8bf758e
Keith Teo <keteo@u91z01s01.42kl.edu.my>
2026-06-23 17:45:14 +0800

    (Addition): main conf content in parsing

--- src/ConfigSrc/ConfigParser.cpp
+++ src/ConfigSrc/ConfigParser.cpp
@@ -1,6 +1,44 @@
-#include "ConfigParser.hpp"
+#include "../../include/ConfigHeader/ConfigParser.hpp"
 
-ServerConfig ConfigParser(std::string configfile)
+ServerConfig	parseServerBlock(const std::vector<std::string> &tokens, size_t &i)
 {
-	if (open(configfile,))
+	ServerConfig config;
+
+	while (i < tokens.size() && tokens[i] != "}")
+	{
+		if (tokens[i] == "listen")
+			config.port = std::atoi(tokens[++i].c_str());
+		else if (tokens[i] == "server_name")
+			config.server_name = tokens[++i];
+		else if (tokens[i] == "root")
+			config.root = tokens[++i];
+		else if (tokens[i] == "location")
+			config.locations.push_back(parseLocationBlock(tokens, i));
+		i++;
+	}
+}
+
+std::vector<ServerConfig>	parse(const std::vector<std::string>& tokens)
+{
+	size_t i = 0;
+	std::vector<ServerConfig> configs;
+
+	while (i < tokens.size())
+	{
+		if (tokens[i] == "server" && tokens[i + 1] == "{")
+		{
+			i += 2;
+			configs.push_back(parseServerBlock(tokens, i));
+		}
+		else
+			throw std::runtime_error("Unexpected token: " + tokens[i]);
+	}
+	
+}
+
+std::vector<ServerConfig>	ConfigParser(std::string configpath)
+{
+	std::vector<std::string> tokens = tokenizer(configpath);
+
+
 }
