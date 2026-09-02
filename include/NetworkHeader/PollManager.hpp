4db54994b38b53b4d8632ecedabc67962805e80e
Qhaby <qhabylen@gmail.com>
2026-09-02 22:44:23 +0800

    Modify PollManager to include server configs and new methods

--- include/NetworkHeader/PollManager.hpp
+++ include/NetworkHeader/PollManager.hpp
@@ -1,17 +1,20 @@
 #ifndef POLLMANAGER_HPP
 # define POLLMANAGER_HPP
+
 # include "../NetworkHeader/ServerSocket.hpp"
 # include "../NetworkHeader/SocketManager.hpp"
 # include "../ParserHeader/ClientConnection.hpp"
 # include <poll.h>
 # include <map>
 # include <sstream>
+# include <fstream>
+# include <fcntl.h>
+# include <vector>
 
 class PollManager
 {
 	public:
-		PollManager();
-		PollManager(SocketManager &manager);
+		PollManager(SocketManager &manager, const std::vector<ServerConfig> &configs);
 		~PollManager();
 		PollManager(const PollManager &other);
 		PollManager &operator=(const PollManager &other);
@@ -21,11 +24,28 @@ class PollManager
 		SocketManager	&_manager;
 		std::vector<pollfd> _poll_fds;
 		std::map<int, ClientConnection> _clients;
+		std::vector<ServerConfig> _configs;
+
+		// Poll management
 		void	addFd(int fd, short events);
 		void	removeFd(int fd);
 		bool	isServerFd(int fd) const;
 		void	handleNewConnections(int server_fd);
-		void	handleClientRead(int client_fd);
+
+		// Client I/O
+		ssize_t	readFromClient(int fd);
+		void	handleClientRead(int fd);
+		void	handleClientWrite(int fd);
+
+		// Request handling
+		void	processRequest(int fd);   // NEW: processes one complete request
+		void	sendResponse(int fd);     // NEW: writes buffer to socket
+
+		// Cleanup
+		void	closeConnection(int fd);
+
+		// Main handler to be implemented by Member 3
+		void	handleRequest(const HttpRequest& req, const std::vector<ServerConfig>& configs, std::string& response);
 };
 
 #endif
